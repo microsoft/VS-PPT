36002c1cc41e1afad3c3b55424fce89a5272b01f
aedampir@gmail.com <aedampir@gmail.com>
2016-03-25 14:23:49 +0700

    Redundant commas were removed.

--- src/CopyAsHtml/CopyAsHtmlTest/HtmlMarkupProviderTests.cs
+++ src/CopyAsHtml/CopyAsHtmlTest/HtmlMarkupProviderTests.cs
@@ -18,7 +18,7 @@ namespace CopyAsHtmlTest
                 { "a\r\nb", 0 },
                 { " a\r\n\r\n b", 1 },
                 { "  a\r\nb", 0 },
-                { "  a\r\n  b other  spaces\t \r\n  baz\t", 2 },
+                { "  a\r\n  b other  spaces\t \r\n  baz\t", 2 }
             };
 
             foreach (var sample in samples)
@@ -40,7 +40,7 @@ namespace CopyAsHtmlTest
                 { "  a\r\n  b other  spaces\t \r\n  baz\t", "a\r\nb other  spaces\t \r\nbaz\t" },
                 { " a\r\n  b \r\n   c\r\n  b\r\n a", "a\r\n b \r\n  c\r\n b\r\na" },
                 { " a\r\n\r\n b", "a\r\n\r\nb" },
-                { " a\r\n \r\n b", "a\r\n\r\nb" },
+                { " a\r\n \r\n b", "a\r\n\r\nb" }
             };
 
             foreach (var sample in samples)
