d427f498e268fa323be245697041271f615142f0
Igor Fesenko <igor.aka.ky7m@gmail.com>
2016-04-25 15:50:31 -0700

    Converts MSTests to XUnit tests.

--- src/CopyAsHtml/CopyAsHtmlTest/FormattedStringBuilderTests.cs
+++ src/CopyAsHtml/CopyAsHtmlTest/FormattedStringBuilderTests.cs
@@ -1,19 +1,18 @@
 ﻿using System.Windows.Media;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Formatting.Implementation;
+using Xunit;
 
 namespace CopyAsHtmlTest
 {
-    [TestClass]
-    public class FormattedStringBuilderTests
+        public class FormattedStringBuilderTests
     {
-        [TestMethod]
+        [Fact]
         public void FormattedStringBuilderEndToEnd1()
         {
             var classificationFormatMap = new MockClassificationFormatMap();
             var htmlMarkupProvider = new HtmlMarkupProvider(
-                classificationFormatMap, 
+                classificationFormatMap,
                 MockClassificationType.Default,
                 Brushes.White);
             var classifier = new MockClassifier();
@@ -25,7 +24,7 @@ namespace CopyAsHtmlTest
                 waitIndicator: null);
 
             var snapshot = new MockTextSnapshot("bla");
-            var spans = new NormalizedSnapshotSpanCollection(new [] 
+            var spans = new NormalizedSnapshotSpanCollection(new []
             {
                 new SnapshotSpan(snapshot, 0, 3)
             });
@@ -33,7 +32,7 @@ namespace CopyAsHtmlTest
             var actualResult = formattedStringBuilder.AppendSnapshotSpans(spans);
             var expectedResult = "<pre style=\"font-family:Consolas;font-size:12;color:black;background:white;\">b<span style=\"color:blue;\">l</span>a\r\n</pre>";
 
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.Equal(expectedResult, actualResult);
         }
     }
 }
--- src/FixMixedTabs/Test/HasMixedTabsUnitTests.cs
+++ src/FixMixedTabs/Test/HasMixedTabsUnitTests.cs
@@ -1,58 +1,57 @@
 ﻿using FixMixedTabs;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace FixMixedTabsUnitTests
 {
-    [TestClass]
-    public class HasMixedTabsUnitTests
+        public class HasMixedTabsUnitTests
     {
-        [TestMethod]
+        [Fact]
         public void TabThen4SpacesTest()
         {
             string text = "\tABC\n    DEF";
-            Assert.IsTrue(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 4, snapshot: new MockSnapshot(text)));
+            Assert.True(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 4, snapshot: new MockSnapshot(text)));
         }
 
-        [TestMethod]
+        [Fact]
         public void SpacesThenTabTest()
         {
             string text = "    ABC\n\tDEF";
-            Assert.IsTrue(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 4, snapshot: new MockSnapshot(text)));
+            Assert.True(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 4, snapshot: new MockSnapshot(text)));
         }
 
-        [TestMethod]
+        [Fact]
         public void NotEnoughSpacesTest()
         {
             string text = "...ABC\n\tDEF";
-            Assert.IsFalse(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 4, snapshot: new MockSnapshot(text)));
+            Assert.False(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 4, snapshot: new MockSnapshot(text)));
         }
 
-        [TestMethod]
+        [Fact]
         public void SpacesTabsMixedOnOneLineTest()
         {
             string text = "  \t  ABC\n\tDEF";
-            Assert.IsTrue(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 2, snapshot: new MockSnapshot(text)));
+            Assert.True(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 2, snapshot: new MockSnapshot(text)));
         }
 
-        [TestMethod]
+        [Fact]
         public void BadTabSizeTest()
         {
             string text = " ABC\n\tDEF";
-            Assert.IsFalse(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 0, snapshot: new MockSnapshot(text)));
+            Assert.False(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 0, snapshot: new MockSnapshot(text)));
         }
 
-        [TestMethod]
+        [Fact]
         public void SpacesNotAtLineStartTest()
         {
             string text = "A  ABC\n\tDEF";
-            Assert.IsFalse(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 2, snapshot: new MockSnapshot(text)));
+            Assert.False(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 2, snapshot: new MockSnapshot(text)));
         }
 
-        [TestMethod]
+        [Fact]
         public void TabsNotAtLineStartTest()
         {
             string text = "A\tABC\n..DEF";
-            Assert.IsFalse(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 2, snapshot: new MockSnapshot(text)));
+            Assert.False(MixedTabsDetector.HasMixedTabsAndSpaces(tabSize: 2, snapshot: new MockSnapshot(text)));
         }
     }
 }
