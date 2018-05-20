ee92b43da16d78a1f0e7b117f5a8c24a1704d581
kirsan31 <17767561+kirsan31@users.noreply.github.com>
2018-05-20 08:05:41 +0300

    Fix for #183 - CopyAsHTMLMarkup font size
    Updated GetFontSize to print "px" on font size.

--- src/CopyAsHtml/CopyAsHtml/HtmlMarkupProvider.cs
+++ src/CopyAsHtml/CopyAsHtml/HtmlMarkupProvider.cs
@@ -381,9 +381,12 @@ namespace Microsoft.VisualStudio.Text.Formatting.Implementation
             return style;
         }
 
+        /// <summary>
+        /// Gets string representation of Font Size in DIPs (Device Independent Pixels). Eg: "12px".
+        /// </summary>
         private static string GetFontSize(TextRunProperties properties)
         {
-            return ((int)properties.FontRenderingEmSize).ToString();
+            return ((int)properties.FontRenderingEmSize).ToString() + "px";
         }
 
         private string GetFontFamily(TextRunProperties properties)
